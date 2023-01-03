d08dd3d511b40d2088da514a95bb96f3a300993b
Akihito Kirisaki <kirisaki@klara.works>
2023-01-04 07:12:56 +0900

    feat: define diagonal

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,11 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Number (sqrt)
 
 main :: Effect Unit
 main = do
   log "🍝"
+
+diagonal :: Number -> Number -> Number
+diagonal x y = sqrt (x * x + y * y)
--- test/Main.purs
+++ test/Main.purs
@@ -3,9 +3,16 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Test.Unit.Assert as Assert
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
+
+import Main
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main = runTest do
+  suite "diagonal" do
+      test "3 4 5" do
+          Assert.equal (diagonal 3.0 4.0) 5.0
+      test "5 12 13" do
+          Assert.equal (diagonal 5.0 12.0) 13.0
